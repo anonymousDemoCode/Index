de080b1055c9cfdd924a59af04384a582644bcc2
Sam <sun.linjin@163.com>
2017-05-23 20:16:47 +0800

    move the democode.sh to Index

---
+++ democode.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# this script used for auto sync the repositories of DemoCode on github
+
+# path control
+declare -r gWorkingDir="$(pwd)"
+declare -r gSelfDir="$(cd "$(dirname "$0")"; pwd)"
+cd "${gSelfDir}"
+
+var=0
+githubRepo=("Index" "C" "Shell_Bash" "Python" "CPP" "Makefile")
+
+#if [ "5CG4380W65" = "$(hostname)" ]; then export https_proxy=http://10.144.1.10:8080; fi
+
+for var in "${githubRepo[@]}"; do
+    if [ -d "${var}" ];then
+        cd "${var}";echo -e "-->> In $(pwd)\n";git pull;git st;cd -;echo -e "\nOut $(pwd) -->>"
+        echo -e "\n\n\n"
+    else
+        git clone https://github.com/anonymousDemoCode/"${var##*/}".git
+    fi
+done
